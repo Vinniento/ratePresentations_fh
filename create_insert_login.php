cddf88908ed1a1f7c985221d57c6703fff2b2c42
Vincent Schauer <schauervincent@gmail.com>
2019-11-17 13:31:23 +0100

    login hinzugefügt --> keine sicherheitsmaßnahmen
    zum testen, voraussetzungen:
    1, auf der phpmyadmin seite: http://localhost:8081/phpmyadmin/index.php?lang=de    --> eine datenbank "ratepresentations" und benutzer "oliver" mit password "nlkj" (solltet ihr noch von früher drin haben).
    2. Ein mal die seite "/create_insert_login.php" aufrufen --> kreiert die tabelle persons und speichert einen teacher und student hinein

---
+++ create_insert_login.php
@@ -0,0 +1,42 @@
+<?php 
+try
+{
+    //create connection to database
+    $conn = new PDO("mysql:host=localhost; dbname=ratepresentations", "oliver", "nlkj");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try {
+
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $create = "CREATE TABLE if not exists `persons` (
+                `person_ID` int not null auto_increment,
+                `firstname` varchar(50) not null,
+                `lastname` varchar(50) not null,
+                `email` varchar(70) not null,
+                `pwd` varchar (50) not null,
+                `isteacher` boolean not null,
+                PRIMARY KEY (`Person_ID`)
+              );";
+            $conn->exec($create);
+            echo "<script>alert('Table \"Person\" created successfully  ')</script>";
+                
+    } catch (PDOException $exception) {
+        echo $create . "<br>" . $exception->getMessage();
+    }
+    //nur für testzwecke
+    $query = "INSERT into persons 
+    (firstname, lastname, email, pwd, isteacher) 
+    values
+    ('teacher', 'test', 'teacher@test.com', 'test', true),
+    ('student', 'test', 'student@test.com', 'test', false) ";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    echo "<script>alert('Successfully inserted into persons ')</script>";
+}
+    catch(PDOException $error) 
+    {
+        echo $error;
+    }
+    ?>
---
+++ login_check.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+/*TODO Hinzufügen von "is submit set usw"
+*/
+
+$email_form = htmlspecialchars($_POST['email']);
+$pass_form = htmlspecialchars($_POST['pwd']);
+
+try
+{
+    //create connection to database
+    $conn = new PDO("mysql:host=localhost; dbname=ratepresentations", "oliver", "nlkj");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try 
+    {
+            //prepare helps against SQL injections
+            $query = "SELECT person_id, email, pwd, isteacher 
+                        FROM persons 
+                            WHERE email = :email";
+            $statement = $conn->prepare($query);
+
+            $statement->bindParam(':email', $email_form);
+
+            $statement->execute();
+
+            //gets row as associative array
+            $users = $statement->fetch(PDO::FETCH_ASSOC);
+            //print_r($users);
+            
+            //if ($email_form === $users['email'] && password_verify($pass_form, $users['pwd']) == true){
+                if ($email_form === htmlspecialchars($users['email']) && $pass_form === htmlspecialchars($users['pwd'])){
+
+                //$_COOKIE["user_id"] = $users["person_id"];
+                $_SESSION['email'] = $email_form;
+                $_SESSION['pwd'] = $pass_form;
+			    $_SESSION['isteacher'] = $users['isteacher'];
+
+                if($users['isteacher']) {
+                header("location: teacher.html"); }
+                else {
+                    header("location: students.html");
+                }
+            }
+            else {
+
+            }
+          
+           
+    }
+    catch(PDOException $error) {
+        echo $error;
+        }
+}
+
+    catch(PDOException $error) {
+    echo $error;
+    }
+    finally {
+        
+    }
+?>
