4b1694e6eebe3a94ad1ebbdeda2aa3e3e53ffc24
Lucas_NB\Lucas01 <lucas@thiess.info>
2019-12-05 16:27:42 +0100

    alter mail code hinzugefügt

--- add_students.php
+++ add_students.php
@@ -2,16 +2,16 @@
 //TODO wenn ein fehler ist --> $message zurück echoen und das ausgeben?
 /* var_dump( $_POST );
 */
-include('random.php');
-if (  isset( $_POST['firstname'] ) && isset( $_POST['lastname'] ) && isset( $_POST['email'] )  ) {
+include( 'random.php' );
+if ( isset( $_POST['firstname'] ) && isset( $_POST['lastname'] ) && isset( $_POST['email'] ) ) {
 
     $firstname = htmlspecialchars( $_POST['firstname'] );
     $lastname = htmlspecialchars( $_POST['lastname'] );
     $email = htmlspecialchars( ( $_POST['email'] ) );
-    $pwd =randcode(8);
-//mail schicken
+    $pass = randcode( 8 );
+    //mail schicken
 
-    $pwd = password_hash((htmlspecialchars($pwd)), PASSWORD_BCRYPT);
+    $pwd = password_hash( ( htmlspecialchars( $pass ) ), PASSWORD_BCRYPT );
     //strtolower
 } else {
 
@@ -33,26 +33,29 @@ try
  {
         //echo "<script>alert('Person already exists  ')</script>";
         header( 'Location: teacher.php' );
-    }
-    else {
+    } else {
         try {
 
             $statement = $conn->prepare( 'INSERT INTO persons (firstname, lastname, email, pwd, isteacher) VALUES (:firstname, :lastname, :email,:pwd, :isteacher)' );
-            $statement->execute( array( 'firstname' => $firstname, 'lastname' => $lastname, 'email' => $email,'pwd' =>$pwd, 'isteacher' => 'false' ) );
+            $statement->execute( array( 'firstname' => $firstname, 'lastname' => $lastname, 'email' => $email, 'pwd' =>$pwd, 'isteacher' => 'false' ) );
+
+            $headers = 'From: webmaster@example.com' . '\r\n' .
+            'Reply-To: webmaster@example.com' . '\r\n' .
+            'X-Mailer: PHP/' . phpversion();
+            $message = 'You have been added as a student:  password ='.$pass;
+
+            $subject = 'project work';
+
+            mail( $email, $subject, $message, $headers );
         } catch ( PDOException $exception ) 
-         {
+ {
             echo $create . '<br>' . $exception->getMessage();
         }
         echo 'added and ' . $result . ' found in db';
-
-
 
     }
 } catch( Exception $ex ) 
  {
     echo 'Error in retrieving existing data for comparison ' . $ex;
 }
-
-
-//header( 'Location: teacher.php' );
 ?>
