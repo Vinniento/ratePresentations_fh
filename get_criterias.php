fbb708ac42cbe4f8fdfc15d6faadfe1c5c80757d
Lucas_NB\Lucas01 <lucas@thiess.info>
2019-12-18 19:34:46 +0100

    + File was zu einen Code alle criterein der presention mit diesen code zurückliefert

---
+++ get_criterias.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+if(isset($_POST['code']))
+{
+    $code=$_POST['code'];
+try{
+
+  
+
+    $query = "SELECT criteria.name FROM  presentations 
+    LEFT JOIN  presentation_to_criteria  ON presentations.presentation_ID = presentation_to_criteria.presentation_ID 
+    LEFT JOIN  criteria  ON presentation_to_criteria.criteria_ID = criteria.criteria_ID  
+    WHERE presentations.code = :code";
+    $statement = $conn->prepare($query);
+    $statement->bindParam(':code', $code);
+
+    $statement->execute();
+
+     //gets row as associative array
+     $criterias = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+     $result = json_encode($criterias);
+     echo $result;
+
+}catch(PDOException $error){
+    echo $error;
+}
+} else{
+    header("location: index.php");
+}
+?>
