be3d7c41bb0c02b1319dfc5f3826d6c0c935d5b6
Lucas_NB\Lucas01 <lucas@thiess.info>
2019-12-18 19:06:47 +0100

    gibt alles zugriffscodes von allesn persentionen die den eingelogten lehrer haben aus

---
+++ get_code.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+try{
+    $email=$_SESSION['email'];
+
+    $query = "SELECT presentations.name, presentations.code FROM  persons 
+    LEFT JOIN  presentation_to_person  ON persons.person_ID = presentation_to_person.person_ID 
+    LEFT JOIN  presentations  ON presentation_to_person.presentation_ID = presentations.presentation_ID  
+    WHERE persons.email = :email";
+    $statement = $conn->prepare($query);
+    $statement->bindParam(':email', $email);
+
+    $statement->execute();
+
+     //gets row as associative array
+     $codes = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+     $students = json_encode($users);
+     echo $students;
+
+
+
+
+
+
+}catch(PDOException $error){
+    echo $error;
+}
+?>
