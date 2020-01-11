db3b5ceee4cb6dd3a38b69e5a1b1a745149280f5
Vincent Schauer <schauervincent@gmail.com>
2020-01-11 12:50:00 +0100

    ratings werden komplett unformatiert angezeigt

--- student.php
+++ student.php
@@ -8,151 +8,111 @@ if($_SESSION['isteacher'] !=0 || (!isset($_SESSION['email']))){
 }
 ?>
 
-    <header class="masthead">
-        <div class="container w-100 h-100 d-flex justify-content-center align-items-center text-center" style="width: 60rem;">
-            <div class="card bg-secondary" style="width: 70rem;">
-                <div class="card-body">
-                    <br>
-                    <div class="row justify-content-center">
-                        <h3 class="card-title">Student bereich</h3>
+<header class="masthead">
+    <div class="container w-100 h-100 d-flex justify-content-center align-items-center text-center"
+        style="width: 60rem;">
+        <div class="card bg-secondary" style="width: 70rem;">
+            <div class="card-body">
+                <br>
+                <div class="row justify-content-center">
+                    <h3 class="card-title">Student bereich</h3>
 
-                        <button style="width: 13rem;" class="btn btn-success badge-pill" type="button" name="view_ratings" value="View Ratings">View my Presentations</button>
-                                <br><br>
-                                <div id="displaypresentations">
-                                    <br>
+                    <button style="width: 13rem;" class="btn btn-success badge-pill" type="button" name="view_ratings"
+                        value="View Ratings">View my Presentations</button>
+                    <br><br>
+                    <div id="displaypresentations">
+                        <br>
 
-                                    <div class="table-responsive-sm">
-                                            <table class="table table-striped table-dark table-hover">
-                                                <thead>
-                                                    <tr>
-                                                        <th>Presentation</th>
-                                                        <th>Date of Presentation</th>
+                        <div class="table-responsive-sm">
+                            <table class="table table-striped table-dark table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Presentation</th>
+                                        <th>Date of Presentation</th>
 
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr v-for="(presentation, index) in presentations">
-                                                        <td>{{presentation.name}}</td>
-                                                        <td>{{presentation.date}}</td>
-                                                        <td><button :id="presentation.presentation_ID" onclick="openRating(this.id)">View Rating</button></td>
-
-
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-
-                                        </div>
-                                </div>
-<!-- ---------------------------------------------------- -->
-<div id="displaycriterias">
-                                <p>Click a presentation to view your ratings</p>
-                                    <br>
-
-                                    <div class="table-responsive-sm">
-                                            <table class="table table-striped table-dark table-hover">
-                                                <thead>
-                                                    <tr>
-                                                        <th>Presentation</th>
-                                                        <th>Date of Presentation</th>
-
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr v-for="(presentation, index) in presentations">
-                                                        <td>{{presentation.name}}</td>
-                                                        <td>{{presentation.date}}</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(presentation, index) in presentations">
+                                        <td>{{presentation.name}}</td>
+                                        <td>{{presentation.date}}</td>
+                                        <td><button :id="presentation.presentation_ID"
+                                                onclick="openRating(this.id)">View Rating</button></td>
 
 
-                                                    </tr>
-                                                </tbody>
-                                            </table>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        </div>
+                    <!-- ---------------------------------------------------- -->
 
-                                        </div>
-                                </div>
-                                <<!-- ---------------------------------------------------- -->
-<div id="displayratings">
-                                <p>Click a presentation to view your ratings</p>
-                                    <br>
-
-                                    <div class="table-responsive-sm">
-                                            <table class="table table-striped table-dark table-hover">
-                                                <thead>
-                                                    <tr>
-                                                        <th>Presentation</th>
-                                                        <th>Date of Presentation</th>
-
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr v-for="(presentation, index) in presentations">
-                                                        <td>{{presentation.name}}</td>
-                                                        <td>{{presentation.date}}</td>
+                    <div id="displayratings">
 
 
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-
-                                        </div>
-                                </div>
+                        <div v-for="(criteria, index) in ratings">
+                            {{index}}
+                            <div v-for="(rating, index) in criteria">
+                                <p>{{rating["AVG(ratings.rating_int)"]}}</p>
+                            </div>
+                        </div>
 
 
                     </div>
                     <br>
                     <script>
-function openRating(id) {
+                    function openRating(id) {
 
-    alert(id);
-}
-
-    var app = new Vue({
-
-        el: '#displaypresentations',
-        data: {
-            presentations: []
-        },
-        mounted() {
-            let vm = this;
-            axios
-                .get('get_presentations_for_show_rating.php')
-                .then(response => {
-                    vm.presentations = response.data;
-                    console.log(vm.presentations);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        }
-    })
-</script>
-
-<script>
-    var app = new Vue({
-
-        el: '#displayratings',
-        data: {
-            ratings: []
-        },
-        mounted() {
-            let vm = this;
-            axios
-                .get('get_criterias_and_ratings.php', {
-                    params: {
-                        id: 90
+                        alert(id);
                     }
 
-                })
-                .then(response => {
-                    vm.ratings = response.data;
-                    console.log(vm.ratings);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        }
-    })
-</script>
-  </body>
-  
-  </html>
+                    var app = new Vue({
 
+                        el: '#displaypresentations',
+                        data: {
+                            presentations: []
+                        },
+                        mounted() {
+                            let vm = this;
+                            axios
+                                .get('get_presentations_for_show_rating.php')
+                                .then(response => {
+                                    vm.presentations = response.data;
+                                    console.log(vm.presentations);
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                });
+                        }
+                    })
+                    </script>
+
+                    <script>
+                    var app = new Vue({
+
+                        el: '#displayratings',
+                        data: {
+                            ratings: []
+                        },
+                        mounted() {
+                            let vm = this;
+                            axios
+                                .get('get_criterias_and_ratings.php', {
+                                    params: {
+                                        id: 2
+                                    }
+
+                                })
+                                .then(response => {
+                                    vm.ratings = response.data;
+                                    console.log(vm.ratings);
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                });
+                        }
+                    })
+                    </script>
+                    </body>
+
+</html>
