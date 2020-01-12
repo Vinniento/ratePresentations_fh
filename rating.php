198ba8bfa8c8b561a60cfa52feac5f70d325b425
cVorlen <christoph.vorlen@stud.fh-campuswien.ac.at>
2020-01-12 18:06:26 +0100

    kleine Anpassung

--- rating.php
+++ rating.php
@@ -39,10 +39,10 @@ if (isset($_POST['code']) || (!isset($_SESSION['email'])) || $_SESSION['isteache
                                     <ul class="contents" v-for="criteria in criterias">
 
                                         <div class="row" style="margin-bottom: 15px;" v-if="criteria.isfeedback === '0'">
-                                            <div class="col-sm-12 col-md-4" style="margin-bottom: 15px;">{{criteria.name}}
+                                            <div class="col-sm-8 col-md-4" style="margin-bottom: 15px;">{{criteria.name}}
                                             </div>
-                                            <div class="col-sm-12 col-md-6" style="margin-bottom: 15px;"><input type="range" class="custom-range" :name="criteria.criteria_ID" :id="criteria.criteria_ID" min="0" value="5" max="10" step="1" v-model="radius"><input type="number" min="0" value="5" max="10" step="1" id="range" class="topcoat-text-input" v-model="radius"></div>
-                                            
+                                            <div class="col-sm-8 col-md-5" style="margin-bottom: 15px;"><input type="range" class="custom-range" :name="criteria.criteria_ID" :id="criteria.criteria_ID" min="0" value="5" max="10" step="1" v-model="radius"></div>
+                                            <div class="col-sm-2 col-md-1"><input type="number" min="0" value="5" max="10" step="1" id="range" class="topcoat-text-input center" v-model="radius"></div>
                                         </div>
                                         <div class="row" style="margin-bottom: 15px;" v-else>
                                             <div class="col-sm-12 col-md-4" style="margin-bottom: 15px;">{{criteria.name}}
